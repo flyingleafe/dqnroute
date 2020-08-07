65c35d1683e8768a003477bafd4076c07e4b3923
Igor Buzhinsky <igor.buzhinsky@gmail.com>
2020-08-07 11:48:49 +0300

    Pretraining in a standalone verification script.

--- notebooks/Verify.py
+++ notebooks/Verify.py
@@ -3,7 +3,7 @@ import torch
 import matplotlib.pyplot as plt
 import argparse
 import yaml
-
+from pathlib import Path
 from tqdm import tqdm
 from typing import *
 
@@ -11,6 +11,7 @@ import os
 current_dir = os.getcwd()
 os.chdir("../src")
 from dqnroute import *
+from dqnroute.networks import *
 os.chdir(current_dir)
 
 parser = argparse.ArgumentParser(description="Verifier of baggage routing neural networks.")
@@ -27,7 +28,150 @@ parser.add_argument("--force_pretrain", action="store_true",
 parser.add_argument("--force_train", action="store_true",
                     help="whether not to load previously saved trained models and force recomputation")
 
+parser.add_argument("--pretrain_num_episodes", type=int, default=10000,
+                    help="pretrain_num_episodes (default: 10000)")
+
 args = parser.parse_args()
+
+
+# 1. load scenario
+scenario = args.config_file
+print(f"Scenario: {scenario}")
+
+with open(scenario) as file:
+    scenario_loaded = yaml.load(file, Loader=yaml.FullLoader)
+
+emb_dim = scenario_loaded["settings"]["router"]["dqn_emb"]["embedding"]["dim"]
+
+# graphs size = #sources + #diverters + #sinks + #(conveyors leading to other conveyors)
+lengths = [len(scenario_loaded["configuration"][x]) for x in ["sources", "diverters", "sinks"]] \
+    + [len([c for c in scenario_loaded["configuration"]["conveyors"].values()
+           if c["upstream"]["type"] == "conveyor"])]
+#print(lengths)
+graph_size = sum(lengths)
+print(f"Embedding dimension: {emb_dim}, graph size: {graph_size}")
+
+
+# 2. pretrain
+
+def pretrain(args, pretrain_dir: str, pretrain_filename: str):
+    """ ALMOST COPIED FROM THE PRETRAINING NOTEBOOK """
+    
+    def gen_episodes_progress(num_episodes, **kwargs):
+        with tqdm(total=num_episodes) as bar:
+            return gen_episodes(bar=bar, num_episodes=num_episodes, **kwargs)
+    
+    class CachedEmbedding(Embedding):
+        def __init__(self, InnerEmbedding, dim, **kwargs):
+            self.dim = dim
+            self.InnerEmbedding = InnerEmbedding
+            self.inner_kwargs = kwargs
+            self.fit_embeddings = {}
+
+        def fit(self, graph, **kwargs):
+            h = hash_graph(graph)
+            if h not in self.fit_embeddings:
+                embed = self.InnerEmbedding(dim=self.dim, **self.inner_kwargs)
+                embed.fit(graph, **kwargs)
+                self.fit_embeddings[h] = embed
+
+        def transform(self, graph, idx):
+            h = hash_graph(graph)
+            return self.fit_embeddings[h].transform(idx)
+    
+    def shuffle(df):
+        return df.reindex(np.random.permutation(df.index))
+    
+    def hash_graph(graph):
+        if type(graph) != np.ndarray:
+            graph = nx.to_numpy_matrix(graph, nodelist=sorted(graph.nodes))
+        m = hashlib.sha256()
+        m.update(graph.tobytes())
+        return base64.b64encode(m.digest()).decode('utf-8')
+    
+    def add_inp_cols(tag, dim):
+        return mk_num_list(tag + '_', dim) if dim > 1 else tag
+
+    def qnetwork_batches(net, data, batch_size=64, embedding=None):
+        n = net.graph_size
+        data_cols = []
+        amatrix_cols = get_amatrix_cols(n)
+        for tag, dim in net.add_inputs:
+            data_cols.append(amatrix_cols if tag == 'amatrix' else add_inp_cols(tag, dim))
+        for a, b in make_batches(data.shape[0], batch_size):
+            batch = data[a:b]
+            addr = batch['addr'].values
+            dst = batch['dst'].values
+            nbr = batch['neighbour'].values
+            if embedding is not None:
+                amatrices = batch[amatrix_cols].values
+                new_btch = []
+                for addr_, dst_, nbr_, A in zip(addr, dst, nbr, amatrices):
+                    A = A.reshape(n, n)
+                    embedding.fit(A)
+                    new_addr = embedding.transform(A, int(addr_))
+                    new_dst = embedding.transform(A, int(dst_))
+                    new_nbr = embedding.transform(A, int(nbr_))
+                    new_btch.append((new_addr, new_dst, new_nbr))
+                [addr, dst, nbr] = stack_batch(new_btch)
+            addr_inp = torch.tensor(addr, dtype=torch.float)
+            dst_inp = torch.tensor(dst, dtype=torch.float)
+            nbr_inp = torch.tensor(nbr, dtype=torch.float)
+            inputs = tuple(torch.tensor(batch[cols].values, dtype=torch.float) for cols in data_cols)
+            output = torch.tensor(batch['predict'].values, dtype=torch.float)
+            yield (addr_inp, dst_inp, nbr_inp) + inputs, output
+
+    def qnetwork_pretrain_epoch(net, optimizer, data, **kwargs):
+        loss_func = nn.MSELoss()
+        for batch, target in qnetwork_batches(net, data, **kwargs):
+            optimizer.zero_grad()
+            output = net(*batch)
+            loss = loss_func(output, target.unsqueeze(1))
+            loss.backward()
+            optimizer.step()
+            yield float(loss)
+
+    def qnetwork_pretrain(net, data, optimizer='rmsprop', epochs=1, save_net=True, **kwargs):
+        optimizer = get_optimizer(optimizer)(net.parameters())
+        epochs_losses = []
+        for i in tqdm(range(epochs)):
+            sum_loss = 0
+            loss_cnt = 0
+            for loss in tqdm(qnetwork_pretrain_epoch(net, optimizer, data, **kwargs), desc=f'epoch {i}'):
+                sum_loss += loss
+                loss_cnt += 1
+            epochs_losses.append(sum_loss / loss_cnt)
+        if save_net:
+            # label changed by Igor:
+            net._label = pretrain_filename
+            net.save()
+        return epochs_losses
+    
+    data_conv = gen_episodes_progress(ignore_saved=True, context='conveyors',
+                                      num_episodes=args.pretrain_num_episodes,
+                                      random_seed=args.random_seed, run_params=scenario,
+                                      save_path='../logs/data_conveyor1_oneinp_new.csv')
+    data_conv.loc[:, 'working'] = 1.0
+    conv_emb = CachedEmbedding(LaplacianEigenmap, dim=emb_dim)
+    args = {'scope': pretrain_dir, 'activation': 'relu', 'layers': [64, 64], 'embedding_dim': conv_emb.dim}
+    conveyor_network_ng_emb = QNetwork(graph_size, **args)
+    conveyor_network_ng_emb_ws = QNetwork(graph_size, additional_inputs=[{'tag': 'working', 'dim': 1}], **args)
+    conveyor_network_ng_emb_losses = qnetwork_pretrain(conveyor_network_ng_emb, shuffle(data_conv), epochs=10,
+                                                       embedding=conv_emb)
+    conveyor_network_ng_emb_ws_losses = qnetwork_pretrain(conveyor_network_ng_emb_ws, shuffle(data_conv), epochs=20,
+                                                          embedding=conv_emb)
+
+pretrain_dir = 'conveyor_test_ng'
+pretrain_filename = f"igor_pretrained_{emb_dim}_{graph_size}_{os.path.split(scenario)[1]}.bin"
+pretrain_path = Path(TORCH_MODELS_DIR) / pretrain_dir / pretrain_filename
+if args.force_pretrain or not pretrain_path.exists():
+    print(f"Pretraining {pretrain_path}...")
+    pretrain(args, pretrain_dir, pretrain_filename)
+else:
+    print(f"Using the already pretrained model {pretrain_path}...")
+
+
+# 3. train
 
 def run_single(file: str, router_type: str, random_seed: int, **kwargs):
     job_id = mk_job_id(router_type, random_seed)
@@ -38,35 +182,9 @@ def run_single(file: str, router_type: str, random_seed: int, **kwargs):
         event_series = runner.run(**kwargs)
     return event_series, runner
 
-# 1. pretrain
 
-def pretrain(args):
-    # TODO read yaml and get parameters
-    scenario = args.config_file
-    
-    with open(scenario) as file:
-        scenario_loaded = yaml.load(file, Loader=yaml.FullLoader)
-    
-    emb_dim = scenario_loaded["settings"]["router"]["dqn_emb"]["embedding"]["dim"]
-    print(emb_dim)
-    
-    # graphs size = #sources + #diverters + #sinks + #(conveyors leading to other conveyors)
-    lengths = [len(scenario_loaded["configuration"][x]) for x in ["sources", "diverters", "sinks"]] \
-        + [len([c for c in scenario_loaded["configuration"]["conveyors"].values()
-               if c["upstream"]["type"] == "conveyor"])]
-    #print(lens)
-    graph_size = sum(lengths)
-    #print(graph_size)
-    # FIXME wrong formula!
-    
-    data_conv = gen_episodes_progress(ignore_saved=True,
-        context='conveyors', num_episodes=10000, random_seed=args.random_seed,
-        run_params=scenario,
-        save_path='../logs/data_conveyor1_oneinp_new.csv')
-    # TODO get graph size with more clever means
-    
-# TODO save/load
-pretrain(args)    
+# TODO train
+# TODO implement smoothing
 
 
 if args.command == "deterministic_test":
