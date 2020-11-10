8dcd3e56a6750ea914f2e2f1a511ef3d35110973
Igor Buzhinsky <igor.buzhinsky@gmail.com>
2020-11-10 20:44:47 +0200

    Added a modified, asymmetric version of the Tarau2010 conveyor graph.

--- src/VerifyWrapper.py
+++ src/VerifyWrapper.py
@@ -5,11 +5,14 @@ def run(command: str, config_file: str, temperature: float, cost_bound: float, m
     os.system(f"ipython Verify.py -- --command {command} --config_file ../launches/igor/{config_file} --softmax_temperature {temperature} --cost_bound {cost_bound} --marabou_path ../../Marabou/build/Marabou {more_args}")
 
 #config = "acyclic_conveyor_energy_test.yaml", 1.5, 43.0 # Mukhutdinov, energy test
-config = "acyclic_conveyor_break_test.yaml",  1.5, 43.0 # Mukhutdinov, conveyor break test
+#config = "acyclic_conveyor_break_test.yaml",  1.5, 43.0 # Mukhutdinov, conveyor break test
 
 #config = "conveyor_cyclic_energy_test.yaml", 1.5, 100000    # Very simple fictitious graph with cycle
 #config = "conveyor_cyclic2_energy_test.yaml", 1.5, 12430.0  # A complication of the previous example
+
 #config = "tarau2010.yaml", 30.0, 972                          # Fictitious graph from the literature
+config = "tarau2010_modified_common.yaml", 15.0, 972           # The same graph, but some conveyor sections were extended, making the graph asymmetric
+
 #config = "johnstone2010.yaml", 3.0, 100000                  # Almost real graph from the literature
 
 #command, command_args = "deterministic_test", ""
@@ -30,7 +33,8 @@ run(command, config[0], config[1], config[2], more_args=command_args+(
     #"--skip_graphviz --verification_lr 0.001"
     #"--force_train"
     #"--skip_graphviz"
-    "--skip_graphviz --force_pretrain --force_train"
-    #"--skip_graphviz --force_train"
+    #"--force_pretrain --force_train"
+    #"--skip_graphviz --force_pretrain --force_train"
+    "--skip_graphviz --force_train"
 ))
 print(f"Elapsed time: {time.time() - start_time:.3f} s")
